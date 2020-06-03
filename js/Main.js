c35a8e9cfa1b00ee69730c8be0aeeb63c2132838
ConnerSherman <55280172+connersherman@users.noreply.github.com>
2020-06-03 09:22:55 +0200

    Start for custom install Prompt. Text is no longer markable and some minor spelling error fixes

--- js/Main.js
+++ js/Main.js
@@ -116,3 +116,32 @@ function decrement(array) {
         array[i][1] = array[i][1] - 1;
     }
 }
+
+var numberOfReloadesWhitoutPrompt = getCookie("numberOfReloadesWhitoutPrompt");
+var d = new Date();
+d.setTime(d.getTime() + (30*24*60*60*1000));
+var expires = "expires="+ d.toUTCString();
+
+if(numberOfReloadesWhitoutPrompt == "" || numberOfReloadesWhitoutPrompt == "0"){
+    showPrompt();
+    document.cookie = "numberOfReloadesWhitoutPrompt" + "=" + 5 + ";" + expires + ";path=/";
+}else {
+    numberOfReloadesWhitoutPrompt--;
+    document.cookie = "numberOfReloadesWhitoutPrompt" + "=" + numberOfReloadesWhitoutPrompt + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
